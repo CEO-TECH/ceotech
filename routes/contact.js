a4ca0996c0fd92c75b4df15cf42a597de3fa16d9
Brainify1 <brianchew357@gmail.com>
2018-10-03 23:34:41 -0400

    dynamic header and footer

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contact.ejs', { title: 'Express' });
+  res.render('contact.ejs', { title: 'Contact' });
 });
 
 module.exports = router;
